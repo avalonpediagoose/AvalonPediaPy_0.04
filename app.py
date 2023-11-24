83d09d3eaade09a38bf58bc6300a22d740cd5887
avalonpediagoose <151800889+avalonpediagoose@users.noreply.github.com>
2023-11-24 16:11:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,14 +29,20 @@ def create_room():
     room_name = request.form.get('room_name')
     
     if room_name not in game_data['game_rooms']:
-        game_data['game_rooms'][room_name] = []
-        
-    return redirect(url_for('lobby'))
+        game_data['game_rooms'][room_name] = {'owner': None, 'players': []}
+
+    # Set the room owner to the player who created the room
+    game_data['game_rooms'][room_name]['owner'] = game_data['players'][-1]
+
+    # Redirect to the newly created room
+    return redirect(url_for('join_room', room_name=room_name))
 
 @app.route('/join_room/<room_name>')
 def join_room(room_name):
-    # 你的處理邏輯
-    return render_template('room.html', room_name=room_name)
+    room_info = game_data['game_rooms'].get(room_name, {})
+    owner = room_info.get('owner', 'Unknown')
+    owner_string = f'房主: {owner}'
+    return render_template('room.html', room_name=room_name, owner_string=owner_string)
 
 @app.route('/reset')
 def reset():
